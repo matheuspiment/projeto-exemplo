0d7220bfc35ccfd74d647bb1b8f94107052c8f75
Matheus Ribeiro Pimenta Nunes <matheuspiment@hotmail.com>
2017-06-11 16:10:47 -0300

    Code Style correction

--- src/main/java/foo/Foo.java
+++ src/main/java/foo/Foo.java
@@ -20,17 +20,17 @@ public class Foo {
     /**
      * Realiza a divisão de dois números.
      *
-     * @param a     Dividendo.
-     * @param b     Divisor.
-     * @return      Resultado da divisão.
+     * @param dividendo     Dividendo.
+     * @param divisor       Divisor.
+     * @return              Resultado da divisão.
      *
      * @throws UnsupportedOperationException Caso o divisor seja 0.
      */
-    public static int div(int a, int b) {
+    public static int div(int dividendo, int divisor) {
         if (b == 0) {
             throw new UnsupportedOperationException("Can't divide by zero!");
         }
 
-        return a / b;
+        return dividendo / divisor;
     }
 }
