d528d4e3751644ec8de102181f7804cb2b10d2d1
matheuspiment <matheuspiment@hotmail.com>
2017-06-11 13:07:38 -0300

    Code Style correction

--- src/test/java/foo/FooTest.java
+++ src/test/java/foo/FooTest.java
@@ -1,15 +1,35 @@
 package foo;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+/**
+ * FooTest class
+ */
 public class FooTest {
 
+    /**
+    * Testa a exceção do divisor 0.
+    *
+    * @param a Dividendo 10.
+    * @param m Divisor 0.
+    *
+    * @throws UnsupportedOperationException Caso o divisor seja 0.
+    */
     @Test(expected = UnsupportedOperationException.class)
     public void testInvalidDivider() throws Exception {
         Foo.div(10, 0);
     }
 
+    /**
+    * Testa a exceção do divisor 0.
+    *
+    * @param a Dividendo 10.
+    * @param m Divisor 5.
+    * @return Resultado da divisão 2.
+    *
+    * @throws UnsupportedOperationException Caso o divisor seja 0.
+    */
     @Test
     public void testAdd() throws Exception {
         assertEquals(Foo.div(10, 5), 2);
