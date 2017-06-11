87ce4e2e26c1802f92cb74c3b3bee1f0b9fb015d
Matheus Ribeiro Pimenta Nunes <matheuspiment@hotmail.com>
2017-06-11 16:58:32 -0300

    Create ProgramaFoo.java

---
+++ src/main/java/foo/ProgramaFoo.java
@@ -0,0 +1,38 @@
+package foo;
+
+import java.util.Random;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Programa que realiza a divisão com base 
+ * no dividendo e divisor informados.
+ */
+public final class ProgramaFoo {
+
+    private static final Logger logger =
+            LogManager.getLogger(ProgramaFoo.class);
+
+    /**
+     * Restringe criação de instância.
+     */
+    private ProgramaFoo() {
+        // Apenas evita criação de instância.
+    }
+
+    /**
+     * Ponto de entrada da aplicação. Apenas para ilustra chamada de método.
+     *
+     * @param args Ignorados.
+     */
+    public static void main(final String[] args) {
+        
+        Random gerador = new Random();
+        logger.info("iniciado");
+        int dividendo = gerador.nextInt();
+        int divisor = gerador.nextInt();
+        int resultado = Foo.div(dividendo, divisor);
+
+        System.out.println("O resultado de " + dividendo + " / " + divisor + " é " + resultado);
+    }
+}
