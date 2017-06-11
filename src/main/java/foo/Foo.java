c5db667791e9fe6a01c3186ec0fb610d5786e8f4
matheuspiment <matheuspiment@hotmail.com>
2017-06-11 12:35:58 -0300

    Compliance with standards

--- src/main/java/foo/Foo.java
+++ src/main/java/foo/Foo.java
@@ -5,11 +5,20 @@ package foo;
  */
 public class Foo {
 
-    public static int div(int a, int b) throws Exception {
-    	if (b == 0) {
-    		throw new UnsupportedOperationException("Can't divide by zero!");
-    	}
-        return a / b;  
+  /**
+  * Realiza a divisão de dois números.
+  *
+  * @param a Dividendo.
+  * @param m Divisor.
+  * @return Resultado da divisão.
+  *
+  * @throws UnsupportedOperationException Caso o divisor seja 0.
+  */
+  public static int div(int a, int b) throws Exception {
+    if (b == 0) {
+      throw new UnsupportedOperationException("Can't divide by zero!");
     }
 
+    return a / b;
+  }
 }
--- src/test/java/foo/FooTest.java
+++ src/test/java/foo/FooTest.java
@@ -5,6 +5,11 @@ import static org.junit.Assert.*;
 
 public class FooTest {
 
+  @Test(expected = UnsupportedOperationException.class)
+  public void testInvalidDivider() {
+    Foo.div(10, 0);
+  }
+
   @Test
   public void testAdd() throws Exception {
   	assertEquals(Foo.div(10, 5), 2);
