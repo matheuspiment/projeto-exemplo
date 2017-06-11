c42274579c2e1f750e8404b531b5458413b70679
Matheus Ribeiro Pimenta Nunes <matheuspiment@hotmail.com>
2017-06-11 13:29:06 -0300

    Code Style correction

--- src/main/java/foo/Foo.java
+++ src/main/java/foo/Foo.java
@@ -6,14 +6,14 @@ package foo;
 public class Foo {
 
     /**
-    * Realiza a divisão de dois números.
-    *
-    * @param a Dividendo.
-    * @param b Divisor.
-    * @return Resultado da divisão.
-    *
-    * @throws UnsupportedOperationException Caso o divisor seja 0.
-    */
+     * Realiza a divisão de dois números.
+     *
+     * @param a     Dividendo.
+     * @param b     Divisor.
+     * @return      Resultado da divisão.
+     *
+     * @throws UnsupportedOperationException Caso o divisor seja 0.
+     */
     public static int div(int a, int b) throws Exception {
         if (b == 0) {
             throw new UnsupportedOperationException("Can't divide by zero!");
